2aa566ac38a0fc27ac2a2304084ad6464872190e
HungryPickle <hungrypicklegit@gmail.com>
2025-04-07 02:20:00 -0400

    remove .idea

--- test_scores.js
+++ test_scores.js
@@ -1,86 +1,92 @@
-let names = ["Ben", "Joel", "Judy", "Anne"];
-let scores = [88, 98, 77, 88];
 
-var $ = function (id) { return document.getElementById(id); };
+let namesArr = ['Ben', 'Joel', 'Judy', 'Anne'];
+let scoresArr = [88, 98, 77, 88];
 
-let avgScores = scores.reduce((a, b) => a + b) / scores.length;
+function getAvgScore() {
+    let i = 0,
+        sum = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        sum += scoresArr[i];
+    }
+    return sum / len;
+}
 
 function getHighScore() {
-	let i=0,max=0,len = scores.length;
-	let name = '';
-	for (i; i < len; i++) {
-		if (scores[i] > max) {
-			max = scores[i];
-			name = names[i];
-		}
-	}
-	return name + ' with score of ' + max;
+    let i = 0,
+        max = 0,
+        len = scoresArr.length;
+    let name = '';
+    for (i; i < len; i++) {
+        if (scoresArr[i] > max) {
+            max = scoresArr[i];
+            name = namesArr[i];
+        }
+    }
+    return name + ' with score of ' + max;
 }
 
-function displayResults(avgScore, highScoreName, highScore) {
-	let results = $('results');
-	if (results.style.display == 'inline') {
-		results.syle.display = 'none';
-	}
-	else {
-		results.style.display = 'inline';
-	}
-	$('avgScore').innerHTML = avgScores;
-	$('highScore').innerHTML = getHighScore();
+function initializeResults() {
+    let results = $('#results');
+    let high = getHighScore();
+    let avg = getAvgScore().toFixed(1);
+    $('#highScore').html(high);
+    $('#avgScore').html(avg);
 }
 
-//  insert a new element in the html table after current index.  Assumes, names and scores are updates already.
-function insertTableElement(scoresTable, rowIndex) {
-	    var rowCount = scoresTable.rows.length;
-		let row = scoresTable.insertRow( rowCount );
-		let name = row.insertCell( 0 );
-		var score = row.insertCell( 1 );
-		name.innerHTML = names[rowCount-1];
-		score.innerHTML = scores[rowCount-1];
-}
 
-// display the scores table if it's not already displayed.  In this case add the table elements to the
-// table
 function displayScores() {
-    if ($("scores").style.display == 'none') {
-		$( "scores" ).style.display = "inline";
-		for (let i = 0; i < scores.length; i++) {
-			insertTableElement( $( "scores_table" ), i );
-		}
-	}
+    // Add your code here
 }
+
+function displayResults() {
+    let results = $('#results');
+    results.toggle();
+}
+
+function insertNewTableElement(newName, newScore) {
+   // Add your code here
+}
+
+function initializeScoresTable() {
+//  Add your code here
+}
+
 
 function addScore() {
-	if ($('score').value == '' || $('name').value == '') {
-		alert("Name and score must have values");
-		return;
-	}
-	displayScores();
-	scores.push(parseInt($("score").value));
-	names.push($("name").value);
-	insertTableElement( $( "scores_table" ), scores.length);
-	$("score").value = "";
-	$("name").value = "";
-	displayResults();
+    let score = $('#score');
+    let name = $('#name');
+    if (score.val() === '' || name.val() === '') {
+        alert('Name and score must have values');
+        return;
+    }
+    scoresArr.push(parseInt(score.val()));
+    namesArr.push($("#name").val());
+    initializeScoresTable();
+    score.val('');
+    name.val('');
+    initializeResults();
+    $('#scores').show();
+    $('#results').show();
 }
 
 window.onload = function () {
-	$("add").onclick = addScore;
-	$("display_results").onclick = displayResults;
-	$("display_scores").onclick = displayScores;
-	var name = $("name");
-	var score = $("score");
+    $('#display_results').on('click',  function() {
+        displayResults();
+    });
 
-	name.addEventListener("keyup", function(event) {
-		if (event.key == "Enter") {
-			$("score").focus();
-		}
-	});
-	score.addEventListener("keyup", function(event) {
-		if (event.key == "Enter") {
-			$("add").click();
-		}
-	});
-};
+    $('#display_scores').on('click',  function() {
+        displayScores();
+    });
+    $('#add').on('click',  function() {
+        addScore();
+    });
 
+    let name = $('#name');
+    let score = $('#score');
 
+    name.focus();
+    initializeResults();
+    initializeScoresTable();
+}
